c25a99bd47c432fe19156160905a12c4fdb320f5
OwenB23 <owenbond23@gmail.com>
2023-09-12 12:20:27 -0400

    Updates to Quiz App

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { useState, useContext } from "react";
+import MainMenu from "./Components/MainMenu";
+import EndScreen from "./Components/EndScreen";
+import Quiz from "./Components/Quiz";
+
+import { QuizContext } from "./Helpers/Contexts";
+
+function App() {
+  const [gameState, setGameState] = useState("menu");
+  const [score, setScore] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Programming Trivia App</h1>
+      <QuizContext.Provider
+        value={{ gameState, setGameState, score, setScore }}
+      >
+        {gameState === "menu" && <MainMenu />}
+        {gameState === "quiz" && <Quiz />}
+        {gameState === "endScreen" && <EndScreen />}
+      </QuizContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Helpers/QuestionBank.js
@@ -0,0 +1,34 @@
+export const Questions = [
+  {
+    prompt: "What is 1 + 3",
+    optionA: "24",
+    optionB: "25",
+    optionC: "22",
+    optionD: "4",
+    answer: "D",
+  },
+  {
+    prompt: "What is your favorite programming language",
+    optionA: "JavaScript",
+    optionB: "Python",
+    optionC: "C#",
+    optionD: "C++",
+    answer: "A",
+  },
+  {
+    prompt: "What is your favori;slakdjfl;aksdjfkl;uage",
+    optionA: "JavaScript",
+    optionB: "Python",
+    optionC: "C#",
+    optionD: "C++",
+    answer: "A",
+  },
+  {
+    prompt: "What is your favorite programming language",
+    optionA: "JavaScript",
+    optionB: "Python",
+    optionC: "C#",
+    optionD: "C++",
+    answer: "A",
+  },
+];
